b801aeb80b622ef2d78a43746a8396145e3edc8e
CuPV <phamvancu19981998@gmail.com>
2026-06-17 23:11:07 +0700

    update barista consumer

--- apps/barista/src/02.adapter/consumer/barista.consumer.ts
+++ apps/barista/src/02.adapter/consumer/barista.consumer.ts
@@ -1,32 +1,51 @@
-// order-consumer.controller.ts
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
 import { Payload, Ctx, RmqContext, EventPattern } from '@nestjs/microservices';
+import {
+  BARISTA_USECASE_TOKEN,
+  type BaristaUseCasePort,
+} from '../../01.domain';
+import { first } from 'rxjs';
 
 @Controller()
 export class BaristaConsumerController {
-
-  constructor() {
-  }
+  constructor(
+    @Inject(BARISTA_USECASE_TOKEN)
+    private readonly baristaUseCase: BaristaUseCasePort,
+  ) {}
 
   @EventPattern('order.paid')
-  handleOrderPaid(@Payload() data: any, @Ctx() context: RmqContext) {
-    console.log('--- [New Message Received] ---');
-    console.log('Event Type: order.paid');
-    console.log('Order Payload Data:', data);
+  async handleOrderPaid(
+    @Payload()
+    data: {
+      orderId: string;
+      memberId: string;
+      source: number;
+      status: number;
+      lines: {
+        id: string;
+        name: string;
+        type: number;
+        price: number;
+        status: number;
+        orderId: string;
+      }[];
+    },
+    @Ctx() context: RmqContext,
+  ) {
     const channel = context.getChannelRef();
     const rawMessage = context.getMessage();
     const routingKey = rawMessage.fields.routingKey;
-    channel.ack(rawMessage);
-    console.log(`Verified Routing Key from RabbitMQ: ${routingKey}`);
-
     try {
-      this.processPaymentSuccess(data);
+      const { lines, orderId } = data;
+      const line = lines[0];
+      await this.baristaUseCase.createOrder({
+        name: line.name,
+        orderId: orderId,
+        type: line.type,
+      });
+      channel.ack(rawMessage);
     } catch (error) {
       console.error('Failed to process paid order:', error);
     }
-  }
-
-  private processPaymentSuccess(orderData: any) {
-    // Business logic placeholder
   }
 }
