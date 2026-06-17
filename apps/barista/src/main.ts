fbab4f6cfa44aea0299b02af4db40c17a339d2da
cuwr <43379226+cupv@users.noreply.github.com>
2026-06-17 17:18:41 +0700

    Update main.ts

--- apps/barista/src/main.ts
+++ apps/barista/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './barista.module';
+import { BaristaModule } from './barista.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const mqUrl = process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672';
+  const queue = process.env.QUEUE || 'order_outbox_queue'
+  const app = await NestFactory.create(BaristaModule);
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [mqUrl],
+      queue: queue,
+      queueOptions: {
+        durable: true,
+      },
+      persistent: true,
+      noAck: false,
+    },
+  });
+
+  const port = process.env.PORT ?? 3000;
+
+  await app.startAllMicroservices()
+
+  app
+    .listen(port)
+    .then(() => {
+      console.log('The application is running on port ', port);
+    })
+    .catch((error) => {
+      console.error('RunApplicationError', error);
+    });
 }
+
 bootstrap();
